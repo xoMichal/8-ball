de50f32dd9338b08de9662ccd76c34d4ddd06d38
Michal Rusek <michal.rusek@dka.io>
2023-02-22 14:20:57 +0100

    8-ball ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+var ballNumber = 1;
+void changeBall() {
+  ballNumber = Random().nextInt(4) + 1;
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                changeBall();
+              }); },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
